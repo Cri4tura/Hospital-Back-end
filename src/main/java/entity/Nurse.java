7a392634e44f1ed4b28c4bd0b8b32b32c09dc271
Ashla-StarWars <167981174+Ashla-StarWars@users.noreply.github.com>
2024-10-18 17:07:40 +0200

    DDBB connection DONE, Hibernate working

--- src/main/java/entity/Nurse.java
+++ src/main/java/entity/Nurse.java
@@ -9,16 +9,21 @@ import jakarta.persistence.Id;
 public class Nurse {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	
+	private int id;
+	
 	private String name;
 	
 	private String password;
-	
-	public Nurse(String name, String password) {
-		super();
-		this.name = name;
-		this.password = password;
+		
+	public Integer getId() {
+		return id;
 	}
 	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+		  
 	public String getName() {
 		return name;
 	}
