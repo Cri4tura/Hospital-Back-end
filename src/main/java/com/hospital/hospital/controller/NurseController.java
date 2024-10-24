329a1aac02b0f8d0d217715e85bbc656c9c97acb
Ashla-StarWars <167981174+Ashla-StarWars@users.noreply.github.com>
2024-10-24 15:25:54 +0200

    apuntando al servidor

--- src/main/java/com/hospital/hospital/controller/NurseController.java
+++ src/main/java/com/hospital/hospital/controller/NurseController.java
@@ -3,12 +3,12 @@ package com.hospital.hospital.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,8 +21,8 @@ import entity.Nurse;
 @RestController
 @RequestMapping("/nurse")
 public class NurseController {
-	@Autowired 
 	
+	@Autowired 
 	private NurseRepository nurseRepository;
 
 	@GetMapping("/index") 
@@ -30,18 +30,14 @@ public class NurseController {
 		return nurseRepository.findAll();
     }
 	
-	
 	@PostMapping("/login")
 	public @ResponseBody boolean login(@RequestParam String name, @RequestParam String password) {
-		 Optional<Nurse> nurse = nurseRepository.validateLogin(name, password);
-		    return nurse.isPresent(); 
-		    
+		Optional<Nurse> nurse = nurseRepository.validateLogin(name, password);
+		return nurse.isPresent(); 
 	}
 	
 	@GetMapping("/name/{name}")
 	public @ResponseBody Optional<Nurse> findByName(@PathVariable("name") String name){
 		return nurseRepository.findByName(name);
 	}
-	
-
 }
