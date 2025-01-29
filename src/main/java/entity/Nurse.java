c782ca7114b3fedde8dc472c3fe5a9c413150391
marti <marti@DESKTOP-Q4EKCTD>
2025-01-29 15:59:41 +0100

    Changed nurse entity

--- src/main/java/entity/Nurse.java
+++ src/main/java/entity/Nurse.java
@@ -1,41 +1,79 @@
 package entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "Nurse")
 public class Nurse {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	
-	private int id;
-	
-	private String name;
-	
-	private String password;
-		
-	public Integer getId() {
-		return id;
-	}
-	
-	public void setId(Integer id) {
-		this.id = id;
-	}
-		  
-	public String getName() {
-		return name;
-	}
-	public void setName(String nombre) {
-		this.name = nombre;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-		
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String password;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date registerDate = new Date();
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
 }
